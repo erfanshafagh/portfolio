86c4690cad921c15a18f07bf14182e6a7eb83833
esa104 <esa104@sfu.ca>
2026-03-22 15:11:01 -0700

    Assets bug

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const portfolioData = {
     name: "Erfan Shafagh",
     role: "Machine Learning Researcher",
     location: "Burnaby, BC, Canada",
-    avatar: "assets/avatar.png",
+    avatar: "assets/avatar.PNG",
     links: [
       { label: "esa104@sfu.ca",                    href: "mailto:esa104@sfu.ca",                          icon: "email"    },
       { label: "linkedin.com/in/erfan-shafagh",    href: "https://www.linkedin.com/in/erfan-shafagh/",    icon: "linkedin" },
